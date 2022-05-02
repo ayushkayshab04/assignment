1317126ace99df66aff2b5e4d0043a4b6bf4425e
Ayush <ayushkayshab04@gmail.com>
2022-05-02 10:12:59 +0530

    Completed

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../app');
+const app = require('../app');
 
 // Assertion style
 
@@ -14,7 +14,7 @@ describe('candidate Api', () => {
 
   describe('GET /candidate', () => {
     it('It should get all the candidates', (done) => {
-      chai.request(server)
+      chai.request(app)
         .get('/candidate')
         .end((err, response) => {
           response.should.have.status(200);
